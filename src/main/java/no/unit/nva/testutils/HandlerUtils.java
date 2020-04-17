44bbfc83bd24dcb9134d0dbd4614bf9b6e2df20e
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-17 14:05:44 +0200

    feat: HandlerUtils

---
+++ src/main/java/no/unit/nva/testutils/HandlerUtils.java
@@ -0,0 +1,34 @@
+package no.unit.nva.testutils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public final class HandlerUtils {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    public static final String BODY_FIELD = "body";
+    public static final String HEADERS_FIELD = "headers";
+
+    public static <T> InputStream requestObjectToInputStream(T requestObject, Map<String, String> headers)
+        throws JsonProcessingException {
+        String requestString= requestObjectToString(requestObject,headers);
+        return new ByteArrayInputStream(requestString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static <T> String requestObjectToString(T requestObject, Map<String, String> headers)
+        throws JsonProcessingException {
+        ObjectNode root = OBJECT_MAPPER.createObjectNode();
+        String requestObjString = OBJECT_MAPPER.writeValueAsString(requestObject);
+        root.put(BODY_FIELD, requestObjString);
+        JsonNode headersNode = OBJECT_MAPPER.convertValue(headers, JsonNode.class);
+        root.set(HEADERS_FIELD, headersNode);
+        return  OBJECT_MAPPER.writeValueAsString(root);
+
+    }
+}
---
+++ src/test/java/no/unit/nva/testutils/HandlerUtilsTest.java
@@ -0,0 +1,35 @@
+package no.unit.nva.testutils;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+
+class HandlerUtilsTest {
+
+    public static final String VALUE = "value";
+
+    @Test
+    public void requestBodyToStringReturnsValidJsonObjectForNullHeaders() throws JsonProcessingException {
+        RequestBody requestBody = new RequestBody();
+        requestBody.setMyField(VALUE);
+        String requestString = HandlerUtils.requestObjectToString(requestBody, null);
+        assertNotNull(requestString);
+    }
+
+
+    private static class RequestBody{
+        private String myField;
+
+        public String getMyField() {
+            return myField;
+        }
+
+        public void setMyField(String myField) {
+            this.myField = myField;
+        }
+
+
+    }
+
+}
